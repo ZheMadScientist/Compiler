b98f11184b58d4aa77b9c211394a904b8e761bbf
The_mad_scientist <zlysenko.makarz@gmail.com>
2017-07-06 22:19:18 +0300

    shet

--- src/Interpretor.java
+++ src/Interpretor.java
@@ -1,57 +1,35 @@
-import org.antlr.runtime.*;
-import org.antlr.v4.codegen.target.CppTarget;
-import org.antlr.v4.runtime.*;
+import org.antlr.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.tree.Tree;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Iterator;
-import java.util.List;
-
 
 /**
- * Created by MakarOn on 05.07.2017.
+ * Created by MakarOn on 06.07.2017.
  */
 public class Interpretor {
 
+    public static void main(String[] args){
+        String input = "struct Pidor{}; \n double b = 1;" + System.lineSeparator() + "struct Pidor{};";
+        CPPgrammarLexer lexer = new CPPgrammarLexer(new ANTLRInputStream(input));
 
-    public static void main(String[] args) throws IOException {
-        Path path = Paths.get("C:\\Javalib\\res\\test.txt");
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-            List<String> temp = Files.readAllLines(path, Charset.forName("UTF-8"));
+        // Pass the tokens to the parser
+        CPPgrammarParser parser = new CPPgrammarParser(tokens);
 
+        CPPgrammarParser.BlockContext tree = parser.block();
 
+        // Specify our entry point
+        // Walk it and attach our listener
+        CPPgrammarBaseListener listener = new Listener();
+        //String code = listener.
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(listener, tree);
 
-            String p = "";
-            for(Iterator<String> i = temp.iterator(); i.hasNext();){
-                p += i;
-            }
-            final String input = p;
-
-
-            ANTLRInputStream inputStream = new ANTLRInputStream(new InputStream() {
-                @Override
-                public int read() throws IOException {
-                    char c = 'p';
-                    for(int i = 0; i < input.length(); i++) {
-                        c = input.charAt(i);
-                    }
-                    return c;
-                }
-            });
-
-            CPPgrammarLexer lexer = new CPPgrammarLexer(inputStream);
-            CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
-            CPPgrammarParser parser = new CPPgrammarParser(commonTokenStream);
-
-            CPPgrammarListener listener = new CPPgrammarBaseListener();
-            listener.enterEveryRule(parser.getRuleContext());
 
     }
 }
